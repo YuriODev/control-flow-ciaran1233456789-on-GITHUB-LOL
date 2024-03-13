2b768e873e333da54ac3d0875882523ba88137b4
Ciaran 😁🤑🤠🤴-silly <161600846+ciaran1233456789-on-GITHUB-LOL@users.noreply.github.com>
2024-03-13 10:54:03 +0000

    solve ex 4 ex 5 ex 6 and ex 7
    solve ex 4 ex 5 ex 6 and ex 7

--- exercises/exercise_5.py
+++ exercises/exercise_5.py
@@ -1,2 +1,14 @@
 # Your solution to Exercise 5
 
+a = int(input("Enter Co-effificent of x^2: "))
+b = int(input("Enter co-efficient of x: "))
+c = int(input("Enter c: "))
+
+discriminant = (b**2) - (4 * a * c)
+
+if discriminant == 0:
+    print("1 Repeated Root")
+elif discriminant > 0:
+    print("2 Real Roots")
+else:
+    print("No Real Roots")
