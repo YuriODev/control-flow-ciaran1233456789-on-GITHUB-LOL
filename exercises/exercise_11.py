26904812131f83436a4a5f4f2e0e33e6294679c7
Ciaran <161600846+ciaran1233456789-on-GITHUB-LOL@users.noreply.github.com>
2024-03-21 09:18:35 +0000

    solve 11

--- exercises/exercise_11.py
+++ exercises/exercise_11.py
@@ -1,2 +1,8 @@
+year = int(input("Enter the year: "))
+
+if year % 4 == 0 and not year % 100 == 0 or year % 400 == 0:
+    print("Leap Year")
+else:
+    print("Ordinary Year")
 # Your solution to Exercise 11
 
